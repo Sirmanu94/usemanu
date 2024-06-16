5100ee2b1eb715a0dc7d76bdbe0f341a9cce421b
Emanuele Avitabile <emanueleavitabile@MBP-di-Emanuele.homenet.telecomitalia.it>
2024-06-16 13:20:25 +0200

    fix

--- src/components/SectionSiti.jsx
+++ src/components/SectionSiti.jsx
@@ -1,4 +1,14 @@
 import shopify from '../assets/shopify.png';
+import copy from "../assets/COPY.png";
+import responsive from "../assets/responsive.png";
+import seo from "../assets/seo.png";
+import marketing from "../assets/marketing.png";
+import performance from "../assets/performance.png";
+import grafica from "../assets/grafica.png";
+
+
+
+
 function SectionSiti(){
     return(
         <>
@@ -14,178 +24,87 @@ function SectionSiti(){
     <div className="mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
     
 
-<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
-        <img className="rounded-t-lg" src={shopify} alt="" />
+        <img className="w-full h-60 object-cover rounded-t-lg" src={copy} alt="" />
     </a>
     <div className="p-5">
         <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Copywriting</h5>
         </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Read more
-             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        Qualunque sia il tuo prodotto o servizio, non puoi rimanere in silenzio sul web, quindi attraverso un piano di comunicazione costruiamo la consapevolezza e la reputazione del tuo prodotto/servizio. </p>
+    </div>
+</div>
+<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <img className="w-full h-60 object-cover rounded-t-lg" src={responsive} alt="" />
+    </a>
+    <div className="p-5">
+        <a href="#">
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Grafica Responsive
+</h5>
         </a>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">I nostri siti web sono perfettamente visibili da computer, smarpthone e tablet. Tutte le immagini sono ad alta definizione e dotate di licenza.</p>
+    </div>
+</div>
+<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <img className="w-full h-60 object-cover rounded-t-lg" src={seo} alt="" />
+    </a>
+    <div className="p-5">
+        <a href="#">
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Indicizzazione SEO
+</h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Il tuo sito sarà ben indicizzato su Google e tutti gli altri motori di ricerca. Ti daremo anche una serie di consigli gratuiti per migliorarne il posizionamento.</p>
+     </div>
+</div>
+<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <img className="w-full h-60 object-cover rounded-t-lg" src={performance} alt="" />
+    </a>
+    <div className="p-5">
+        <a href="#">
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Alte prestazioni</h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">I nostri server sono dotati di hardware ad altissime prestazioni. Il tuo sito sarà caricato in pochissimi secondi da qualsiasi zona del mondo.</p>
+  
+    </div>
+</div>
+<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <img className="w-full h-60 object-cover rounded-t-lg" src={marketing} alt="" />
+    </a>
+    <div className="p-5">
+        <a href="#">
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Marketing</h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Offriamo implementazione di plug-in per il marketing che ti renderanno facile aumentare la fidelizzazione dei tuoi utenti.</p>
+    </div>
+</div>
+<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <img className="w-full h-60 object-cover rounded-t-lg" src={grafica} alt="" />
+    </a>
+    <div className="p-5">
+        <a href="#">
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Grafiche e Design</h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Il servizio prevede progettazione logo e immagine coordinata del brand; realizzazione dell’intera grafica per campagne pubblicitarie, sempre coerente con il core del brand.
+
+</p>
+
     </div>
 </div>
 
-      <div className="flex items-start gap-4">
-        <span className="shrink-0 rounded-lg bg-gray-800 p-4">
-          <svg
-            className="h-5 w-5"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-            <path
-              d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-            ></path>
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-            ></path>
-          </svg>
-        </span>
-
-        <div>
-          <h2 className="text-lg font-bold">COPYWRITING</h2>
-
-          <p className="mt-1 text-sm text-gray-300">
-          Qualunque sia il tuo prodotto o servizio, non puoi rimanere in silenzio sul web, quindi attraverso un piano di comunicazione costruiamo la consapevolezza e la reputazione del tuo prodotto/servizio.?
-          </p>
-        </div>
-      </div>
-
-      <div className="flex items-start gap-4">
-        <span className="shrink-0 rounded-lg bg-gray-800 p-4">
-          <svg
-            className="h-5 w-5"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-            <path
-              d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-            ></path>
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-            ></path>
-          </svg>
-        </span>
-
-        <div>
-          <h2 className="text-lg font-bold">ALTE PRESTAZIONI</h2>
-
-          <p className="mt-1 text-sm text-gray-300">
-          I nostri server sono dotati di hardware ad altissime prestazioni. Il tuo sito sarà caricato in pochissimi secondi da qualsiasi zona del mondo.
 
 
-          </p>
-        </div>
-      </div>
 
-      <div className="flex items-start gap-4">
-        <span className="shrink-0 rounded-lg bg-gray-800 p-4">
-          <svg
-            className="h-5 w-5"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-            <path
-              d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-            ></path>
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-            ></path>
-          </svg>
-        </span>
 
-        <div>
-          <h2 className="text-lg font-bold">INDICIZZAZIONE SEO</h2>
+    
 
-          <p className="mt-1 text-sm text-gray-300">
-          Il tuo sito sarà ben indicizzato su Google e tutti gli altri motori di ricerca. Ti daremo anche una serie di consigli gratuiti per migliorarne il posizionamento.          </p>
-        </div>
-      </div>
-
-      <div className="flex items-start gap-4">
-        <span className="shrink-0 rounded-lg bg-gray-800 p-4">
-          <svg
-            className="h-5 w-5"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-            <path
-              d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-            ></path>
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-            ></path>
-          </svg>
-        </span>
-
-        <div>
-          <h2 className="text-lg font-bold">MARKETING</h2>
-
-          <p className="mt-1 text-sm text-gray-300">
-            Implementazione di plug-in che ti renderanno semplice e facile da gestire la gestione delle tue maijl pubblicitarie per migliorare la fidelizzazione dell'utente.
-          </p>
-        </div>
-      </div>
-
-      <div className="flex items-start gap-4">
-        <span className="shrink-0 rounded-lg bg-gray-800 p-4">
-          <svg
-            className="h-5 w-5"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-            <path
-              d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-            ></path>
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-            ></path>
-          </svg>
-        </span>
-
-        <div>
-          <h2 className="text-lg font-bold">SICUREZZA</h2>
-
-          <p className="mt-1 text-sm text-gray-300">
-          Gli attacchi hacker ti preoccupano? Sei in una botte di ferro! I nostri siti web sono dotati di Certificato SSL. La privacy dei tuoi clienti sarà sempre al sicuro.          </p>
-        </div>
-      </div>
+    
     </div>
   </div>
 </section>
