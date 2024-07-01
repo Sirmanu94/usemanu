ba5909a6479a2a97174d58c358c218b7dc4a1ea2
Emanuele Avitabile <emanueleavitabile@MBP-di-Emanuele.homenet.telecomitalia.it>
2024-07-01 20:58:35 +0200

    ref

--- src/components/usePageTracking.jsx
+++ src/components/usePageTracking.jsx
@@ -1,19 +1,26 @@
-import { useLocation } from "react-router-dom";
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga4';
 
 function usePageTracking() {
-    const location = useLocation();
-  
-    useEffect(() => {
-      // Traccia la visualizzazione della pagina
-      ReactGA.send({ hitType: "pageview", page: location.pathname + location.search });
-  
-      // Traccia un evento personalizzato per ogni cambio di rotta
-      ReactGA.send({
-        hitType: 'event',
-        eventCategory: 'Navigation',
-        eventAction: 'Route Change',
-        eventLabel: location.pathname
-      });
-    }, [location]);
-  }
-  export default usePageTracking;
+  const location = useLocation();
+
+  useEffect(() => {
+    // Traccia la visualizzazione della pagina
+    ReactGA.send({ hitType: 'pageview', page: location.pathname + location.search });
+
+    // Ottieni il riferimento da cui l'utente è arrivato
+    const referrer = document.referrer || 'Direct';
+
+    // Traccia un evento personalizzato per ogni cambio di rotta
+    ReactGA.send({
+      hitType: 'event',
+      eventCategory: 'Navigation',
+      eventAction: 'Route Change',
+      eventLabel: location.pathname,
+      eventValue: referrer  // Aggiungi il referer come eventValue
+    });
+  }, [location]);
+}
+
+export default usePageTracking;
