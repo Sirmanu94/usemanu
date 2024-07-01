2cf2632c9a258b062c623467970ad8ace5e3f151
Emanuele Avitabile <emanueleavitabile@MBP-di-Emanuele.homenet.telecomitalia.it>
2024-07-01 19:28:32 +0200

    track 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { useEffect } from 'react';
-import './App.css'
-import Home from './pages/Home'
-import ReactGA from 'react-ga';
-ReactGA.initialize('G-Z78FMWF9FH');
+import './App.css';
+import Home from './pages/Home';
+import ReactGA from 'react-ga4';
 
+// Inizializzare con l'ID di misurazione di GA4
+ReactGA.initialize('G-Z78FMWF9FH');
 
 function App() {
   useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    // Traccia la visualizzazione della pagina
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
   }, []);
   
   return (
     <>
-  <Home/>
+      <Home/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
