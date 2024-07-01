2e2fb55a1ad22299d2009b0fbe5d6a250fa75104
Emanuele Avitabile <emanueleavitabile@MBP-di-Emanuele.homenet.telecomitalia.it>
2024-07-01 19:51:27 +0200

    we

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,16 @@ function App() {
   useEffect(() => {
     // Traccia la visualizzazione della pagina
     ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
+
+    // Traccia un evento personalizzato per la visualizzazione della Home
+    if (window.location.pathname === '/' || window.location.pathname === '/home') {
+      ReactGA.send({
+        hitType: 'event',
+        eventCategory: 'Page View',
+        eventAction: 'Home View',
+        eventLabel: 'Home Page'
+      });
+    }
   }, []);
   
   return (
